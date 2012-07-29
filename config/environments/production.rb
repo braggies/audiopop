2266f5feb6a87ccd9bfb137845d552405ba36ba9
Ryan Braganza <ryan.braganza@gmail.com>
2012-07-29 17:56:39 +1000

    rails g devise:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Audiopop::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } # TODO update url
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
