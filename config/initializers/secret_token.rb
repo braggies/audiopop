3c2b69eaa676d5fff8290b499a18bdff7c609ccd
Ryan Braganza <ryan.braganza@gmail.com>
2012-07-29 16:47:14 +1000

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audiopop::Application.config.secret_token = '110d51201173c503f70b4c2686425e55cd165e9307c1887e4488782b7c1de764d4a56bd6bbcd4074939b3c81af9a8610b4051d4c2bd9b7968995d055c5c5e7bb'
